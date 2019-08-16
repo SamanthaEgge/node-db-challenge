3378b4f2a6419c77adb32e58be5f4ecf6baa60db
unknown <samantha.c.egge@gmail.com>
2019-08-16 09:10:53 -0700

    GET/POST routes written out for tasks and projects.

---
+++ task/task-router.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const Tasks = require('./task-model.js')
+
+const router = express.Router();
+
+router.get('/', async (request, response) => {
+  try {
+    const tasks = await Tasks.find()
+    response.json(tasks)
+  } catch (error) {
+    console.log(error)
+    response.status(500).json({ message: 'Failed to retrieve tasks from the server' })
+  }
+})
+
+router.post('/', async (request, response) => {
+  const newTask = request.body
+
+  try {
+    const task = await Tasks.add(newTask)
+    response.json(task)
+  } catch (error) {
+    console.log(error)
+    response.status(500).json({ message: 'Failed to add new task to the server' })
+  }
+})
