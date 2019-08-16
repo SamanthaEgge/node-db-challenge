d9b78ece354b110234c0c7b55aca1dde039aafa2
unknown <samantha.c.egge@gmail.com>
2019-08-16 08:47:26 -0700

    Tables only need true/false logic now. Wrote out GET/POST for projects.

--- data/migrations/20190816082550_create-tables.js
+++ data/migrations/20190816082550_create-tables.js
@@ -7,7 +7,26 @@ exports.up = function(knex) {
         .unique()
         .notNullable();
       tbl.text('project_description');
-      
+    })
+    .createTable('tasks', tbl => {
+      tbl.increments();
+      tbl.text('task_description')
+        .notNullable();
+      tbl.text('task_notes')
+      tbl.integer('project')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    })
+    .createTable('resources', tbl => {
+      tbl.increments();
+      tbl.text('resources_name')
+        .notNullable()
+        .unique()
+      tbl.text('resources_description')
     })
 };
 
---
+++ project/project-router.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const Projects = require('./scheme-model.js')
+
+const router = express.Router();
+
+router.get('/', async (request, response) => {
+  try {
+    const projects = await Projects.find()
+    response.json(projects)
+  } catch (error) {
+    console.log(error)
+    response.status(500).json({ message: 'Failed to retrieve projects from the server' })
+  }
+})
+
+router.post('/', async (request, response) => {
+  const newProject = request.body
+
+  try {
+    const project = await Projects.add(newProject)
+    response.json(project)
+  } catch (error) {
+    console.log(error)
+    response.status(500).json({ message: 'Failed to add new project to the server' })
+  }
+})
