b26e600104d17d80742f0f0c4202b7ab9c730959
unknown <samantha.c.egge@gmail.com>
2019-08-16 08:13:58 -0700

    Initialized project with npm. Created Index.js and Server.js files.

---
+++ index.js
@@ -0,0 +1,9 @@
+const server = require("./server.js");
+
+const port = 8000;
+const greeting = "Hi, Samantha!";
+server.listen(port, () => {
+  console.log(
+    `\n*** ${greeting} Server Running on http://localhost:${port} ***\n`
+  );
+});
